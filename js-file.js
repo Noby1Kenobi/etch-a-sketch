3f68a606ff51747cf56d9858c0e615929d1854c0
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2022-12-08 13:26:27 -0500

    Add function to generate 16x16 grid of divs

--- js-file.js
+++ js-file.js
@@ -1,1 +1,13 @@
-const container = document.querySelector("container");
+const container = document.querySelector("#container");
+
+// generate 16x16 
+function generateGrid() {
+    for (let i = 0; i < 16*16; i++) {
+        let gridItem = document.createElement("div");
+        gridItem.textContent = i+1;
+        gridItem.className = "grid-item";
+        container.appendChild(gridItem);
+    }
+}
+
+generateGrid();
